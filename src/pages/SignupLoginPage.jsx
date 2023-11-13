ccfa0c945b4697cd90fe465f1ab9818c3eee23cc
souravmndl24 <srvmandal24@gmail.com>
2023-11-14 01:08:58 +0530

    Added styling to homepage

--- src/pages/SignupLoginPage.jsx
+++ src/pages/SignupLoginPage.jsx
@@ -1,16 +1,21 @@
 import LoginForm from "../components/core/Auth/LoginForm";
 import SignupForm from "../components/core/Auth/SignupForm";
 import '../css/pages/SignupLoginPage.css'
+import Title from '../assets/images/TitleText.png'
 
-const SignupLoginPage = () =>{
-    let logged = true;
-    return(
-       <div className="Container-SL">
-       {logged?<LoginForm className="loginPage"/>:<SignupForm className="signPage"/>}
-           
-           
-       </div>
-    )
-   }
+const SignupLoginPage = () => {
+   return (
+      <div className="SLPage">
+         <div className="MainBox">
+            <div className="TitleText"><img src={Title} alt="YGGDRASIL'S CHRONICLES"/></div>
+            <div className="SignupLogin">
+               <button className="Signup">Signup</button>
+               <button className="Login">Login</button>
+            </div>
+               <button className="Guest"><span>Continue as Guest?</span></button>
+         </div>
+      </div>
+   )
+}
    
    export default SignupLoginPage; 
