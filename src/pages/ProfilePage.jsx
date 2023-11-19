f737fdbfe41b55da6fcfc99e217fe718430acd87
souravmndl24 <srvmandal24@gmail.com>
2023-11-19 19:44:21 +0530

    added styles to homepage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,25 +1,23 @@
-import '../css/pages/ProfilePage.css'
-import { useLocation } from 'react-router-dom';
+import "../css/pages/ProfilePage.css";
+import { useLocation } from "react-router-dom";
 
 const ProfilePage = () => {
-
-    // const user = JSON.parse(localStorage.getItem("user"));
-    const location= useLocation();
-    const { user } = location.state; // Read values passed on state
-
+  // const user = JSON.parse(localStorage.getItem("user"));
+  const location = useLocation();
+  const { user } = location.state; // Read values passed on state
 
   return (
-    <div className='profile'>
-        <div className="user">
+    <div className="profile">
+      <div className="user">
         <img src={user.image} alt="" />
-        <h1>{user.firstName} {user.lastName}</h1>
+        <h1>
+          {user.firstName} {user.lastName}
+        </h1>
         <h2>{user.email}</h2>
         <p>{user.followers.length}</p>
-
-        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default ProfilePage; 
+export default ProfilePage;
